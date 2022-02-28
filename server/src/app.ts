1ec510d33d7a7b47b31b1efcef32858691e1b7fd
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-02-28 12:15:48 -0600

    Initial project setup with Typescript and Express

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
