f58e88fbb5165f7e882a3411e9620d2e1ca6d21c
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-03-28 18:58:55 -0600

    Modifies Reta model

--- server/src/models/Reta.ts
+++ server/src/models/Reta.ts
@@ -2,21 +2,24 @@ import { Document, Model, model, Types, Schema } from "mongoose"
 import { IUser } from "./User"
 
 export interface IReta {
-    name: string,
-    description: string,
-    date: Date,
-    hours: number,
-    minutes: number,
-    duration: number,
-    location: string,
-    private: boolean,
-    min_participants?: number,
-    max_participants?: number,
-    confirmed_users: IUser[]
+    name: string;
+    description: string;
+    date: Date;
+    hours: number;
+    minutes: number;
+    duration: number;
+    location: string;
+    private: boolean;
+    min_participants?: number;
+    max_participants?: number;
+    confirmed_users: IUser[];
+    admin: Types.ObjectId;
+    active: boolean;
 }
 
 interface IRetaDocument extends IReta, Document {
     confirmed_users: Types.Array<IUser>;
+    admin: Types.ObjectId;
 }
 // TMethodsAndOverrides
 type RetaDocumentProps = {
@@ -29,14 +32,16 @@ const RetaSchema: Schema<IRetaDocument, RetaModelType> = new Schema<IRetaDocumen
     name: { type: String, required: [true, "Name is missing!"] },
     description: { type: String, required: [true, "No description was provided!"] },
     date: { type: Date, required: [true, "No date was provided!"] },
-    hours: { type: Number, required: [true, "No start time was provided!"]},
-    minutes: { type: Number, required: [true, "No start minutes were provided!"] },
-    duration: { type: Number, required: [true, "No duration was provided!"] },
+    hours: { type: Number, required: [true, "No start time was provided!"], min: 0, max: 23},
+    minutes: { type: Number, required: [true, "No start minutes were provided!"], min: 0, max: 59 },
+    duration: { type: Number, required: [true, "No duration was provided!"], min: 1},
     location: { type: String, required: [true, "No location was provided!"] },
     private: { type: Boolean, default: false },
     min_participants: { type: Number, min: 2 },
     max_participants: { type: Number, min: 2 },
-    confirmed_users: { type: [{ type: Types.ObjectId , ref: "User" }]} // check if this actually works
+    confirmed_users: { type: [{ type: Types.ObjectId , ref: "User" }]}, // check if this actually works
+    admin: { type: Schema.Types.ObjectId, ref: "User", required: [true, "Need an admin for the event"] },
+    active: { type: Boolean, default: true }
 });
 
 const RetaModel = model<IRetaDocument>('Reta', RetaSchema);
