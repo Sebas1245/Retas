369e97d3e0aa52fc9a5ec08ed48b660439167b19
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-04-03 21:29:46 -0500

    Adds starter for React client

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='bg-primary'>Hello there</div>
+      <div className='bg-secondary'>Hello there</div>
+      <div className='bg-dark'><p className='text-light'>Hello there</p></div>
+    </div>
+  );
+}
+
+export default App;
