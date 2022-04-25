510d11040cb8617e8e4f7ecb9a5979d510fcfc48
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-04-25 17:18:31 -0500

    Adds error handling middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,25 +1,35 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express, { json, Request, Response } from 'express';
+import cors from 'cors';
 import setupMongoDB from './services/dbConfig';
 import UserRoutes from './controllers/User';
 import RetasRoutes from './controllers/Retas';
+import sendAsJson from './middleware/sendAsJson';
+import errorHandler from './middleware/errorHandler';
 
 const app = express();
 const PORT = 8080 || process.env.PORT;
 
 app.use(json())
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
 
+// setup DB
 setupMongoDB();
 
-// User routes 
-app.use('/user', UserRoutes)
-app.use('/retas', RetasRoutes)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello from app.ts!');
 });
 
-app.listen(PORT, () => {
-    return console.log(`Express is listening at http://localhost:${PORT}`);
-});
+// User routes 
+app.use('/user', UserRoutes)
+// Retas routes
+app.use('/retas', RetasRoutes)
+
+// error handling middleware
+app.use(errorHandler());
+app.use(sendAsJson());
+
+app.listen(PORT, () => console.log(`Express is listening at http://localhost:${PORT}`));
--- server/src/middleware/sendAsJson.ts
+++ server/src/middleware/sendAsJson.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import CustomError from './customError'
 
-const sendAsJson = () => async (err: CustomError, req: Request, res: Response) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const sendAsJson = () => (err: CustomError, req: Request, res: Response, next: NextFunction) => {
     console.log('err: ', err);
     if (err.statusCode !== 500 ) {
         return res.status(err.statusCode).json({ code: err.statusCode, name: err.name, message: err.message });
