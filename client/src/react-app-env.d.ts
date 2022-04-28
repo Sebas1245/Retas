91c30c895ccf0ec7cd35aa6d5a532bda4762f6da
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-04-27 19:38:05 -0500

    Makes types global and fixes environment variable setup

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        SERVER_URL: string
+      }
+    }
+  }
+  
+  // If this file has no import/export statements (i.e. is a script)
+  // convert it into a module by adding an empty export statement.
+  export {}
