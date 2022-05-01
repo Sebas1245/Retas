549cbac0754d4218c4ef94c2e18ab64f67e57753
Angel-AG <miniprobecat@hotmail.com>
2022-05-01 14:02:25 -0500

    Add client side validation to login view

--- client/src/views/HomeLogin.tsx
+++ client/src/views/HomeLogin.tsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeGuest from "../components/HomeGuestBase";
 import Form from "../components/Form";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
+import { login } from "../services/userCalls";
 
 export default function HomeLogin() {
+
+  const navigate = useNavigate()
+  const [usernameFeedback, setUsernameFeedback] = useState("")
+  const [passwordFeedback, setPasswordFeedback] = useState("")
+
+  const onSubmit: (e: React.FormEvent) => void = async (e) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      usuario: { value: string },
+      password: { value: string },
+    };
+
+    let errorFound = false;
+
+    if (!target.usuario.value) {
+      setUsernameFeedback("Escribe tu nombre de usuario.")
+      errorFound = true
+    } else {
+      setUsernameFeedback("")
+    }
+
+    if (!target.password.value) {
+      setPasswordFeedback("Escribe tu contraseña.")
+      errorFound = true
+    } else {
+      setPasswordFeedback("")
+    }
+
+    if (errorFound) {
+      return;
+    }
+
+    try {
+      await login(target.usuario.value, target.password.value)
+      navigate("/")
+    } catch (error) {
+      const err = error as typeof error & ErrorResponse;
+      setPasswordFeedback(err.msg)
+    }
+  }
+
   return (
     <HomeGuest 
         imgSrc="./portero_retas.jpg" 
@@ -13,17 +56,19 @@ export default function HomeLogin() {
         linkMsg="Crear cuenta"
         navigateTo="/register"
         >
-      <Form className="row mt-5 pt-4">
+      <Form className="row mt-5 pt-4" onSubmit={onSubmit} noValidate={true}>
         <Input type="text" divClass="form-floating col-lg-7 mb-4" inputClass="form-control rounded-pill"
             inputId="usuario" placeholder="Usuario" labelClass="form-label ps-4" 
+            feedbackClass="px-3 pt-2 text-danger" feedbackText={usernameFeedback}
         />
         <Input type="password" divClass="form-floating col-lg-7 mb-4" inputClass="form-control rounded-pill"
             inputId="password" placeholder="Contraseña" labelClass="form-label ps-4" 
+            feedbackClass="px-3 pt-2 text-danger" feedbackText={passwordFeedback}
         />
         <div className="d-grid col-lg-7 mb-4">
           <Button 
                 className="btn-primary rounded-pill fw-bold"
-                btnType="button"
+                btnType="submit"
                 btnText="Iniciar sesión"
                 padding="py-3"/>
         </div>
