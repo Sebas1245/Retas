ee2cbdbdc123be0857b5bcf3abe95298d902cbf4
Axel Rodríguez <A01570152@itesm.mx>
2022-05-02 00:42:05 -0500

    Style changes to New Reta screen

--- client/src/views/NewReta.tsx
+++ client/src/views/NewReta.tsx
@@ -41,59 +41,35 @@ export default function NewReta() {
                             />
                             <div className="row mb-5">
                                 <h6 style={{color:"#e5e5e5", fontWeight:"bold",textAlign:"left", paddingBottom:5}}>Datos generales</h6>
-                                <Input type="url" divClass="form-floating col-lg-4" inputClass="form-control rounded-pill border-dark border-2"
+                                <Input type="url" divClass="form-floating col-lg-4" inputClass="form-control ps-3 rounded-pill border-dark border-2"
                                     inputId="loc" placeholder="Seleccionar ubicación" labelClass="form-label ps-4" 
                                 />
-                                <Input type="date" divClass="form-floating col-lg-4" inputClass="form-control rounded-pill border-dark border-2"
+                                <Input type="date" divClass="form-floating col-lg-4" inputClass="form-control ps-3 rounded-pill border-dark border-2"
                                     inputId="date" placeholder="Seleccionar fecha" labelClass="form-label ps-4" 
                                 />
                             </div>
                             <div className="row mb-5">
                                 <h6 style={{color:"#e5e5e5", fontWeight:"bold",textAlign:"left", paddingBottom:5}}>Jugadores</h6>
                                 <div className="form-floating col-lg-4">
-                                    <select className="form-control rounded-pill ps-4 border-dark border-2">
+                                    <select className="form-control rounded-pill ps-3 pt-2 border-dark border-2">
                                         <option selected hidden>Elegir privacidad</option>
                                         <option value="1">Privado</option>
                                         <option value="2">Público</option>
                                     </select>
                                 </div>
-                                <div className="form-floating col-lg-4">
-                                    <select className="form-control rounded-pill ps-4 border-dark border-2" id="exampleFormControlSelect1">
-                                        <option selected hidden className="form-label">Mínimo de jugadores</option>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                        <option>6</option>
-                                        <option>7</option>
-                                        <option>8</option>
-                                        <option>9</option>
-                                        <option>10</option>
-                                    </select>
-                                </div>
-                                <div className="form-floating col-lg-4">
-                                    <select className="form-control rounded-pill ps-4 border-dark border-2" id="exampleFormControlSelect2">
-                                        <option selected hidden className="form-label">Máximo de jugadores</option>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                        <option>6</option>
-                                        <option>7</option>
-                                        <option>8</option>
-                                        <option>9</option>
-                                        <option>10</option>
-                                    </select>
-                                </div>
+                                <Input type="number" divClass="form-floating col-lg-4" inputClass="form-control ps-3 rounded-pill border-dark border-2"
+                                    inputId="minimo" placeholder="Mínimo de jugadores" labelClass="form-label ps-4" 
+                                />
+                                <Input type="number" divClass="form-floating col-lg-4" inputClass="form-control ps-3 rounded-pill border-dark border-2"
+                                    inputId="maximo" placeholder="Máximo de jugadores" labelClass="form-label ps-4" 
+                                />
                             </div>
                             <div className="row mb-5">
                                 <h6 style={{color:"#e5e5e5", fontWeight:"bold",textAlign:"left", paddingBottom:5}}>Adicionales</h6>
-                                <Input type="time" divClass="form-floating col-lg-4" inputClass="form-control rounded-pill border-dark border-2"
+                                <Input type="time" divClass="form-floating col-lg-4" inputClass="form-control rounded-pill ps-3 border-dark border-2"
                                     inputId="conf" placeholder="Hora límite de confirmación" labelClass="form-label ps-4" 
                                 />
-                                <Input type="number" divClass="form-floating col-lg-4" inputClass="form-control rounded-pill border-dark border-2"
+                                <Input type="number" divClass="form-floating col-lg-4" inputClass="form-control ps-3 rounded-pill border-dark border-2"
                                     inputId="costo" placeholder="Costo por admisión" labelClass="form-label ps-4" 
                                 />
                             </div>
@@ -108,7 +84,7 @@ export default function NewReta() {
                                 <div className="d-grid col-lg-4">
                                     <Button 
                                         className="btn-primary rounded-pill fw-bold"
-                                        btnType="button"
+                                        btnType="submit"
                                         btnText="Crear Reta"
                                         padding="py-3"/>
                                 </div>
