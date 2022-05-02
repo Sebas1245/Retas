34be4f1f1f6972d51eafe803b827b6e330766c96
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-05-02 18:01:45 -0500

    Adds correct calls to my profile

--- client/src/views/UserProfile.tsx
+++ client/src/views/UserProfile.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from '../components/Navbar';
 import Sidebar from "../components/Sidebar";
 import Flush from "../components/Flush";
 import CardGrid from "../components/CardGrid/CardGrid";
+import { getAllRetasForUser } from "../services/userCalls";
 
 export default function UserProfile() {
+    const [retas, setRetas] = useState<Array<Reta>>();
+
+    useEffect(() => {
+        const fetchRetas = async () => {
+        try {
+            const {retas} = await getAllRetasForUser();
+            setRetas(retas);
+        } catch (error) {
+            alert(JSON.stringify(error));
+        }
+        }
+        fetchRetas();
+    }, []);
     return (
         <div className="full-page-with-nav">
-            <Navbar />
-            <div className="row full-page-with-nav">
+            <div className="row" style={{height: '100vh'}}>
                 <div className="d-none d-md-block col-12 col-lg-3">
                     <Sidebar
                         title="Mi Perfil"
@@ -22,7 +35,7 @@ export default function UserProfile() {
                     </Sidebar>
                 </div>
                 <div className="col-12 col-lg-9">
-                    <CardGrid category="" />
+                    <CardGrid retas={retas} />
                 </div>
             </div>
         </div>
