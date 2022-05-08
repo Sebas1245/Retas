46e7d8f0faa464b4b556f280e410dc3cfe6ae178
Angel-AG <miniprobecat@hotmail.com>
2022-05-07 21:13:15 -0500

    Make category cards smaller

--- client/src/components/CardGrid/CardGrid.tsx
+++ client/src/components/CardGrid/CardGrid.tsx
@@ -1,19 +1,15 @@
-import React, { useEffect, useState } from "react"
-import { getAllRetas, getAllRetasByCategory } from "../../services/retaCalls"
+import React from "react"
 import { getWeekday, getMonth, formatTime } from "../../utils/dateTransforms";
 import { getImageByCategory } from "../../utils/imageCategory";
 import Card from "./Card"
-import LinkNav from "./LinkNav"
 
 export default function CardGrid({ retas }: { retas: Array<Reta> | undefined }) {
-
-
   return (
     <div className="row h-100 ">
       <div className='row row-cols-1 row-cols-md-3 g-3'>
         {
           retas?.map(reta => (
-            <div className="col-md-4 col-sm-6">
+            <div key={reta._id ? reta._id : ""} className="col-md-4 col-sm-6">
               <Card
                 retaId={reta._id ? reta._id : ""}
                 imgSource={getImageByCategory(reta.category)}
