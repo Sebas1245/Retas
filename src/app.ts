88d5bb3da5476ccee7952573345b9d933e83b9ae
Angel-AG <miniprobecat@hotmail.com>
2022-05-18 19:35:47 -0500

    Add login view. No implementation yet.

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import path from 'path';
 const app = express();
 const PORT = 8080 || process.env.PORT;
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: false}));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
